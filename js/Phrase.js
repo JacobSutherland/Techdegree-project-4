42a9b1699476fc48332ce67d39665502aa850039
JacobSutherland <jacobsutherland224@gmail.com>
2020-10-30 03:59:57 -0500

    added phrase class and required methods

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,44 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+     constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+     }
+     //Parses the classes phrase value by iterating each letter and creating an LI element / adding the appropriate classes to each LI
+     addPhraseToDisplay(){
+            const phrase = document.querySelector('#phrase ul');
+            for(let char of this.phrase){
+                if(char !== ' '){
+                const letter = document.createElement('li');
+                letter.textContent = char;
+                letter.classList.add('hide', 'letter', char)
+                phrase.appendChild(letter)
+                } else {
+                const space = document.createElement('li');
+                space.classList.add('space');
+                phrase.appendChild(space)
+            }
+        }
+    }
+    //Checks whether the phrase includes a players guess
+    checkLetter(letter){
+    if(this.phrase.includes(letter)){
+        return true
+        } else {
+        return false
+        }
+    }
+    //Dsiplays matched values by adding the 'show' class to LI elements with the same class names as a users guesses
+    showMatchedLetter(letter){
+        const matched = document.getElementsByClassName(letter);
+        for(let i = 0; i < matched.length; i++){
+            matched[i].classList.add("show");
+            matched[i].classList.remove("hide");
+            console.log(matched[i])
+        }
+    }
+}
+
+

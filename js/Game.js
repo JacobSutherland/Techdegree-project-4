9a2b3419823fca796ec3ea5a0a1dd6f0e2c15c81
JacobSutherland <jacobsutherland224@gmail.com>
2020-11-18 01:33:05 -0600

    added game reset, refactored handleInteraction function and created Game instance in app.js

--- js/Game.js
+++ js/Game.js
@@ -1,14 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
-
-const startBtn = document.querySelector('#btn__reset');
 const overlay = document.querySelector('#overlay');
+const scoreboard = document.querySelector('#scoreboard ol');
+const hearts = document.querySelectorAll('#scoreboard li img');
+const keyboard = document.querySelector('#qwerty');
+
 let phraseOne = new Phrase('mario is cool');
 let phraseTwo= new Phrase('kirby');
 let phraseThree = new Phrase('link');
 let phraseFour = new Phrase('samus');
 let phraseFive = new Phrase('yoshi');
+
  class Game {
      constructor(){
         this.missed = 0;
@@ -28,69 +31,73 @@ let phraseFive = new Phrase('yoshi');
         this.activePhrase = currentPhrase;
         return currentPhrase;
      }
-     //applies the appropriate CSS class based on correct or incorrect selection by comparing the key's (target) text  to letters inside the game objects active phrase, finally shows letters or removes hearts via methods.
-     handleInteraction(){
-      const keyboard = document.querySelector('#qwerty');
-
-      keyboard.addEventListener('click', e => {
-         if(e.target.className === 'key'){
-            const key = e.target;
-            key.disabled = true;
-             
-               if(this.activePhrase.phrase.includes(key.textContent)){
-                  key.classList.add('chosen')
-                  this.activePhrase.showMatchedLetter(key.textContent);
-                  this.checkForWin();
-               } else {
-                  key.classList.add('wrong')
-                  this.removeLife();
-               }
+     //Assigns the appropriate class based on right or wrong guesses and disables key to corresponding guesses, then calls function to evaluate win or loss each turn.
+   handleInteraction(key){
+      key.disabled = true;
+         if(this.activePhrase.phrase.includes(key.textContent)){
+            key.classList.add('chosen')
+            this.activePhrase.showMatchedLetter(key.textContent);
+            this.checkForWin();
+         } else {
+            key.classList.add('wrong')
+            this.removeLife();
          }
-      });
-     }
+}
+   //Increments the Games's missed property on each call from handleInteraction() until 5 while updating the hearts images
      removeLife(){
-      const scoreboard = document.querySelector('#scoreboard ol');
-      const hearts = document.querySelectorAll('#scoreboard li img');
-      const body = document.querySelector('body')
       this.missed += 1;
       hearts[this.missed - 1].src = '/images/lostHeart.png'
       if(this.missed === 5){
          this.gameOver('GOOD TRY, TAKE ANOTHER SHOT');
       }
      }
+     //Checks each letter of the current phrase, if it contains classes belonging to guessed letters including spaces, a count is incremented until the count is equal to the length of the phrase indicating a win.
      checkForWin(){
       const phrase = document.querySelectorAll('#phrase li');
       let correct = 0;
-      let endGame = false;
-      console.log(phrase)
         for(let letter of phrase){
-            if(letter.classList.contains('show')){
+            if(letter.classList.contains('show')||letter.classList.contains('space')){
                correct += 1;
-               console.log(correct)
             }
             if(correct === phrase.length && this.missed < 5){
-               endGame = true;
                this.gameOver('WINNER');
             }
          }
      }
+     //delegates the resukts of the game by displaying results based on where the function is called (removeLife () or CheckForWin()) where the corresponding result is passed as an argument. Also clears the game to initial starting state.
      gameOver(result){
       const gameOverMessage = document.querySelector('#game-over-message');
+      const phraseUl = document.querySelector('#phrase ul');
+      const keyrows = document.querySelectorAll('.keyrow button');
       overlay.classList.remove('start');
       gameOverMessage.textContent = result;
-      overlay.style.display = 'initial';
       if(result === 'WINNER'){
+         overlay.classList.remove('lose')
          overlay.classList.add('win');
       } else {
+         overlay.classList.remove('win')
          overlay.classList.add('lose');
       }
+
+      //game reset's by undoing all manipulated keys, properties, hearts, or related classes.
+      overlay.style.display = 'initial';
+      while(phraseUl.firstChild){
+      phraseUl.removeChild(phraseUl.firstChild);
+      }
+      for(let key of keyrows){
+         key.classList.remove('wrong', 'chosen', 'show');
+         key.disabled = false;
+      }
+      for(let heart of hearts){
+         heart.src = '/images/liveHeart.png'
+      }
+      this.missed = 0;
      }
  }
 
 
- startBtn.addEventListener('click', () => {
-   let game = new Game();
-   game.startGame();
-   game.handleInteraction();
- })
- 
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+const startBtn = document.querySelector('#btn__reset');
+
+let game = new Game();
+
+startBtn.addEventListener('click', () => {
+    game.startGame();
+  })
+
+  keyboard.addEventListener('click', (e) => {
+    if(e.target.className === 'key'){
+       const key = e.target;
+    game.handleInteraction(key);
+    console.log(key);
+    }
+  })
